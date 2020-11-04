abc57446040718579567fe5a2f7690663d5b2c2a
arjunQ21 <arjunadhikari789@gmail.com>
2020-11-04 10:36:44 +0545

    first commit

---
+++ BisectionMethod.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#define EPS 0.001
+
+int iteration = 0;
+
+float f(float x){
+	return (x - sin(x) - 0.5) ;
+}
+float bisection(float a, float b){
+	if(f(a)*f(b) < 0){
+		float c = (a + b) / 2 ;
+		float error = fabs(a - b) ;
+		printf("Iteration: %d ", ++iteration) ;
+		printf("a: %f, b: %f, c: %f, f(a): %f, f(b): %f, f(c): %f, Error: %f\n", a, b, c, f(a), f(b), f(c), error) ;
+
+		if(error < EPS) {
+			printf("\n\nEnd.\nx= %f", c);
+			return c ;
+		}
+		if(f(a)*f(c) < 0){
+			 bisection(a, c) ;
+		}else{
+			 bisection(b, c) ;
+		}
+	}else{
+		printf("invalid range: (%.2f, %.2f)", a, b) ;
+		return 0.0 ;
+	}
+}
+main(){
+//	float a, b, c, fa, fb, fc, error ;
+float a, b ;
+printf("\nEnter range ");
+	printf("%d", bisection(1, 3)) ;
+}
+
+
